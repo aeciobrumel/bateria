2dd1ef2c1aa7c605a0d3ebedb2b7b1b1fb78ad50
Aécio Brumel <88293649+aeciobrumel@users.noreply.github.com>
2025-06-15 01:12:29 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 document.body.addEventListener("keyup", (event) => {
-  playSound(event.code.toLowerCase());
+  play(event.code.toLowerCase());
+});
+document.querySelector(".composer button").addEventListener("click", () => {
+  playComposer();
 });
 
-function playSound(sound) {
-  let keyElement = document.querySelector(`#${sound}`);
-  console.log(sound);
+function play(sound) {
+  let keyElement = document.querySelector(`div[data-key="${sound}"]`);
+  let audioElement = document.querySelector(`#s_${sound}`);
+
+  if (audioElement) {
+    playAudio(audioElement);
+    keystyle(keyElement);
+  }
+}
+
+function playAudio(e) {
+  e.currentTime = 0;
+  e.play();
+}
+
+function keystyle(e) {
+  e.classList.add("active");
+  setTimeout(() => {
+    e.classList.remove("active");
+  }, 300);
+}
+
+function playComposer() {
+  let song = document.querySelector("#input").value;
+  if (song !== " ") {
+    let musica = song.split("");
+    wait = 0;
+    for (let i of musica) {
+      setTimeout(() => {
+        play(`key${i}`);
+      }, wait);
+      wait += 250;
+    }
+  }
 }
